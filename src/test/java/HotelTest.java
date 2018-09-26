e05fd8e085575667184bda7bb1b87fa41f1eb178
Shannon Holmes <snholmes13@gmail.com>
2018-09-26 13:20:52 +0100

    update add family bedroom (bedroom 3)

--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -10,6 +10,7 @@ public class HotelTest {
     ArrayList<Bedroom> bedrooms;
     Bedroom bedroom1;
     Bedroom bedroom2;
+    Bedroom bedroom3;
 
     @Before
     public void before() {
@@ -17,8 +18,10 @@ public class HotelTest {
         hotel = new Hotel("The KodeKlan Kitty Kat Hotel", bedrooms);
         bedroom2 = new Bedroom (1, 2, "single", 10.00);
         bedroom1 = new Bedroom (2, 1, "double", 19.99);
+        bedroom3 = new Bedroom (4, 1, "family", 24.99);
         bedrooms.add(bedroom1);
         bedrooms.add(bedroom2);
+        bedrooms.add(bedroom3);
 
     }
 
@@ -29,6 +32,6 @@ public class HotelTest {
 
     @Test
     public void getRoomCount() {
-        assertEquals(2, hotel.getRoomCount());
+        assertEquals(3, hotel.getRoomCount());
     }
 }
