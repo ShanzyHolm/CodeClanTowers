885cf659728e01db1e628dea70759555384975b5
Shannon Holmes <snholmes13@gmail.com>
2018-09-26 13:13:32 +0100

    updated Hotel name

--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -14,7 +14,7 @@ public class HotelTest {
     @Before
     public void before() {
         bedrooms = new ArrayList<>();
-        hotel = new Hotel("The Kitty Kat Hotel", bedrooms);
+        hotel = new Hotel("The KodeKlan Kitty Kat Hotel", bedrooms);
         bedroom2 = new Bedroom (1, 2, "single", 10.00);
         bedroom1 = new Bedroom (2, 1, "double", 19.99);
         bedrooms.add(bedroom1);
